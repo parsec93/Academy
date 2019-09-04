7ee1d1cae4d875db2a5aad4eb24e3632799521d0
Administer <Administer@Administer-PC>
2019-09-05 01:28:17 +0900

    교사리스트, 상세정보

--- Academy/WebContent/admin/adminMain.jsp
+++ Academy/WebContent/admin/adminMain.jsp
@@ -9,7 +9,7 @@
 <body>
 <h1>관리자 페이지</h1>
 
-<input type="button" value="교사관리" onclick="location.href='joinAdminForm.jsp'">
+<input type="button" value="교사관리" onclick="location.href='../TeacherList.me'">
 <input type="button" value="수업관리" onclick="location.href='../lectureList.le'">
 
 </body>
--- Academy/src/dao/MemberDAO.java
+++ Academy/src/dao/MemberDAO.java
@@ -1,11 +1,15 @@
 package dao;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import static db.JdbcUtil.*;
 
+import vo.BoardBean;
 import vo.MemberBean;
 
 public class MemberDAO {
@@ -227,6 +231,114 @@ public class MemberDAO {
 	    
 	    return updateCount;
 	   }
+	   
+		public int selectListCount(String member_isMember) {
+			int listCount = 0; // 선생님 인원수를 저장하는 변수
+
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+				System.out.println(member_isMember);
+				String sql = "SELECT COUNT(*) FROM member WHERE member_isMember=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, member_isMember);
+				rs = pstmt.executeQuery();
+
+				if (rs.next()) {
+					listCount = rs.getInt(1);
+				}
+
+			} catch (SQLException e) {
+				System.out.println("selectListCount() 에러 - " + e.getMessage());
+			} finally {
+				// static import 문을 사용하여 JdbcUtil 클래스명 지정 필요없음
+				close(rs);
+				close(pstmt);
+			}
+
+			return listCount;
+		}
+	   
+	   public ArrayList<MemberBean> selectTeacherList(String member_isMember){
+		   	PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			ArrayList<MemberBean> teacherList = new ArrayList<MemberBean>();
+			
+			try {
+				// SELECT 구문 : board 테이블 데이터 전체 조회
+				// => board_re_ref 기준 내림차순, board_re_seq 기준 오름차순
+				// => 전체 갯수가 아닌 시작 레코드 번호 ~ limit 갯수 만큼 읽어오기
+				String sql = "SELECT * FROM member WHERE member_isMember=? ORDER BY member_teacher_code";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, member_isMember);
+				rs = pstmt.executeQuery();
+
+				// ResultSet 객체 내의 모든 레코드를 각각 레코드별로 BoardBean 에 담아서 ArrayList 객체에 저장
+				// => 패스워드 제외
+				while (rs.next()) {
+					MemberBean memberBean = new MemberBean();
+					memberBean.setMember_teacher_code(rs.getString("member_teacher_code"));
+					memberBean.setMember_name(rs.getString("member_name"));
+					memberBean.setMember_id(rs.getString("member_id"));
+					memberBean.setMember_email(rs.getString("member_email"));
+					memberBean.setMember_postcode(rs.getString("member_postcode"));
+					memberBean.setMember_add1(rs.getString("member_add1"));
+					memberBean.setMember_add2(rs.getString("member_add2"));
+					memberBean.setMember_phone(rs.getString("member_phone"));
+
+
+					teacherList.add(memberBean);
+				}
+
+			} catch (SQLException e) {
+				System.out.println("selectArticleList() 에러 - " + e.getMessage());
+			} finally {
+				close(rs);
+				close(pstmt);
+			}
+
+			return teacherList;
+			
+			
+	   }
+	   
+	   public MemberBean selectTeacher(String member_teacher_code) { // 글 번호를 전달받아 조회 후, 결과를 BoardBean 으로 리턴
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			MemberBean memberBean = null;
+
+			try {
+				// board_num 에 해당하는 게시물 조회 후, 결과값을 BoardBean 에 저장하여 리턴
+				String sql = "SELECT * FROM member WHERE member_teacher_code=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, member_teacher_code);
+				rs = pstmt.executeQuery();
+
+				if (rs.next()) { // 조회된 게시물이 있을 경우
+					memberBean = new MemberBean();
+					memberBean.setMember_name(rs.getString("member_name"));
+					memberBean.setMember_id(rs.getString("member_id"));
+					memberBean.setMember_email(rs.getString("member_email"));
+					memberBean.setMember_pass(rs.getString("member_pass"));
+					memberBean.setMember_postcode(rs.getString("member_postcode"));
+					memberBean.setMember_add1(rs.getString("member_add1"));
+					memberBean.setMember_add2(rs.getString("member_add2"));
+					memberBean.setMember_phone(rs.getString("member_phone"));
+					memberBean.setMember_teacher_code(rs.getString("member_teacher_code"));
+//					memberBean.setMember_date(rs.getDate("member_date"));
+				}
+			} catch (SQLException e) {
+				System.out.println("selectArticle() 에러 - " + e.getMessage());
+			} finally {
+				close(rs);
+				close(pstmt);
+			}
+
+			return memberBean;
+
+		}
 	
 	
 }
