46b5d5620751357c6c9ced3d0abf8e0971eba351
dhzmfzl <rnjsalst@gmail.com>
2019-10-01 20:30:23 +0900

    장바구니 추가

--- src/svc/member/MemberBasketService.java
+++ src/svc/member/MemberBasketService.java
@@ -1,9 +1,14 @@
 package svc.member;
 
+import dao.LectureDAO;
 import dao.MemberDAO;
+import vo.LectureBean;
+
 import static db.JdbcUtil.*;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberBasketService {
 	public void insertBasket(String member_id, String lecture_id) {
@@ -19,5 +24,18 @@ public class MemberBasketService {
 		close(con);
 		
 		return;
+	}
+	
+	public List<LectureBean> selectBasketList(String sId){
+		System.out.println("바스켓서비스");
+		Connection con = getConnection();
+		LectureDAO lectureDAO = LectureDAO.getInstance();
+		lectureDAO.setConnection(con);
+		List<LectureBean> lectureList=new ArrayList<LectureBean>();
+		lectureList = lectureDAO.getBasketList(sId);
+		
+		close(con);
+		
+		return lectureList;
 	}
 }
